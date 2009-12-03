0a31bd443cc0a9cde026f46add5cf1dc4493a5cd
svart.riddare <svart.riddare@80e46872-9bb2-11de-bc28-6fc548ead51f>
2009-12-03 21:04:46 +0000

    Meilleure intégration avec Linux.

--- branches/archives/Source Code/UnixSystem.cpp
+++ branches/archives/Source Code/UnixSystem.cpp
@@ -1,10 +1,29 @@
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include "System.h"
 
 /*************************************************************/
 
 unsigned int MemoireDisponible()
 {
-	return 0;
+	FILE *Fichier = fopen("/proc/meminfo", "r");
+
+	unsigned int Memoire = 0;
+	char Ligne[128];
+
+	if (Fichier) {
+		while (fgets(Ligne, 128, Fichier))
+			if (sscanf(Ligne, "MemFree: %u kB", &Memoire) == 1)
+				break;
+
+		fclose(Fichier);
+	}
+
+	if (Memoire <= 0)
+		return 0;
+
+	return 10 + log(Memoire) / log(2);
 }
 
 /*************************************************************/
