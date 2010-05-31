0f5082b05a492efe26c5bc63142035400a97e4a1
svart.riddare <svart.riddare@80e46872-9bb2-11de-bc28-6fc548ead51f>
2010-05-31 19:29:50 +0000

    Version 0.97. Disponible sur Google Code Hosting.

--- branches/archives/Source Code/Version.h
+++ branches/archives/Source Code/Version.h
@@ -1,6 +1,6 @@
 #ifndef __VERSION_H
 #define __VERSION_H
 
-#define EUCLIDE_VERSION  "Euclide 0.96, ©2000-2010 Étienne Dupuis"
+#define EUCLIDE_VERSION  "Euclide 0.97, ©2000-2010 Étienne Dupuis"
 
 #endif
