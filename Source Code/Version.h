b953d695addbfaeb0c2666a3b82f7e5540a233dd
svart.riddare <svart-riddare@users.noreply.github.com>
2010-06-01 19:35:25 +0000

    Préparation de la version 0.98.

--- Source Code/Version.h
+++ Source Code/Version.h
@@ -1,6 +1,6 @@
 #ifndef __VERSION_H
 #define __VERSION_H
 
-#define EUCLIDE_VERSION  "Euclide 0.97, ©2000-2010 Étienne Dupuis"
+#define EUCLIDE_VERSION  "Euclide 0.98, ©2000-2010 Étienne Dupuis"
 
 #endif
