42b80471dccaec126f21d58915603ce05bac0444
svart.riddare <svart.riddare@80e46872-9bb2-11de-bc28-6fc548ead51f>
2010-06-01 19:36:32 +0000

    Version 0.98. Disponible sur Google Code Hosting.

---
+++ tags/version-0.98/Source Code/Erreur.cpp
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Erreur.h"
+#include "Output.h"
+
+/*************************************************************/
+
+void ErreurFichierLangue(const char *Fichier)
+{
+	unsigned int L = strlen(Fichier);
+
+	char *Tampon = new char[L + 8];
+	memcpy(Tampon, Fichier, L);
+	Tampon[L++] = ' ';
+	Tampon[L++] = '?';
+	Tampon[L++] = '?';
+	Tampon[L++] = '?';
+	Tampon[L++] = '\a';
+	Tampon[L] = '\0';
+
+	OutputMessageErreur(Tampon);
+	OutputDestroy();
+	
+	fprintf(stderr, "%s\n", Tampon);
+
+	delete[] Tampon;
+	exit(-1);
+}
+
+/*************************************************************/
+
+void FatalOutputError()
+{
+	fprintf(stderr, "\n\n\t\aOutput fatal error!!!\n\n");
+	abort();
+}
+
+/*************************************************************/
+
+void ErreurEnonce(texte Erreur)
+{
+	OutputMessageErreur(Erreur);
+	throw Erreur;
+}
+
+/*************************************************************/
+
+void ErreurInterne(const char *Fichier, unsigned int Ligne)
+{
+	unsigned int i = 0, j = 0;
+	char FichierNu[1024];
+
+	while (Fichier[i]) {
+		FichierNu[j++] = Fichier[i];
+
+		if ((Fichier[i] == '/') || (Fichier[i] == '\\'))
+			j = 0;
+
+		i++;                                                           
+	}
+	
+	FichierNu[j] = '\0';
+
+	char Tampon[128];
+	sprintf(Tampon, "%s (%u) %s", FichierNu, Ligne, GetTexte(MESSAGE_ERREURINTERNE, 64, false));
+
+	OutputMessageErreur(Tampon);
+
+	texte Exception = MESSAGE_ERREURINTERNE;
+
+	throw Exception;
+}
+
+/*************************************************************/
