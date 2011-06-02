0c0d3b1d28c7b312f53b8262799ed8c516829dce
svart.riddare@gmail.com <svart.riddare@gmail.com@80e46872-9bb2-11de-bc28-6fc548ead51f>
2011-06-02 14:11:36 +0000

    Implémentation Linux du commit précédent.

--- branches/archives/Source Code/UnixSystem.cpp
+++ branches/archives/Source Code/UnixSystem.cpp
@@ -1,6 +1,8 @@
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 #include "System.h"
 
 /*************************************************************/
@@ -30,7 +32,20 @@ unsigned int MemoireDisponible()
 
 const char *CheminAlternatif(const char *Fichier)
 {
-	return Fichier;
+	static char Chemin[512];
+	char Process[64];
+
+	memset(Chemin, 0, sizeof(Chemin));
+	sprintf(Process, "/proc/%d/exe", (int)getpid());
+	if (readlink(Process, Chemin, sizeof(Chemin) - 1) < 0)
+		return Fichier;
+
+	char *Executable = strrchr(Chemin, '/');
+	if (!Executable)
+		return Fichier;
+
+	strncpy(Executable + 1, Fichier, sizeof(Chemin) - (Executable - Chemin) - 2);
+	return Chemin;
 }
 
 /*************************************************************/
