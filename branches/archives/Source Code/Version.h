ba0f075853775f1837e2ca81c3454e3938e173d7
svart.riddare@gmail.com <svart.riddare@gmail.com@80e46872-9bb2-11de-bc28-6fc548ead51f>
2014-03-01 10:50:11 +0000

    Préparation de la version 1.00.

--- branches/archives/Source Code/Version.h
+++ branches/archives/Source Code/Version.h
@@ -1,6 +1,6 @@
 #ifndef __VERSION_H
 #define __VERSION_H
 
-#define EUCLIDE_VERSION  "Euclide 0.99, ©2000-2011 Étienne Dupuis"
+#define EUCLIDE_VERSION  "Euclide 1.00, ©2000-2014 Étienne Dupuis"
 
 #endif
