bc9a9c298c5a8d2b42b1b3dd7a5313ec7d3bea86
Étienne Dupuis <e.dupuis@ateme.com>
2021-07-11 15:01:57 +0200

    Ajout des problèmes de test de natch 3.3.

--- problems/run-all.bat
+++ problems/run-all.bat
@@ -5,6 +5,7 @@ set timeout=60
 set problems=
 set problems=%problems%;no-captures.txt
 set problems=%problems%;single-capture.txt
+set problems=%problems%;natch-test-problems.txt
 set problems=%problems%;bichromatic.txt
 set problems=%problems%;edupuis.txt
 
